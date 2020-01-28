509a0334bc7da5851d273b1e926d5d96be504b81
unknown <sarah.vieda@labinfo.is.escuelaing.edu.co>
2020-01-28 18:50:28 -0500

    spark

--- src/main/java/edu/escuelaing/arep/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/SparkWebApp.java
@@ -6,6 +6,8 @@
 package edu.escuelaing.arep;
 
 import static spark.Spark.*;
+import spark.Request;
+import spark.Response;
 /**
  *
  * @author sarah.vieda
@@ -15,7 +17,36 @@ public class SparkWebApp {
     public static void main (String[]args){
         port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");
+        get("/inputdata", (req, res) -> inputDataPage(req, res));
+        get("/results", (req, res) -> resultsPage(req, res));
     }
+    
+    private static String inputDataPage(Request req, Response res) {
+        String pageContent
+                = "<!DOCTYPE html>"
+                + "<html>"
+                + "<body>"
+                + "<h2>HTML Forms</h2>"
+                + "<form action=\"/results\">"
+                + "  First name:<br>"
+                + "  <input type=\"text\" name=\"firstname\" value=\"Mickey\">"
+                + "  <br>"
+                + "  Last name:<br>"
+                + "  <input type=\"text\" name=\"lastname\" value=\"Mouse\">"
+                + "  <br><br>"
+                + "  <input type=\"submit\" value=\"Submit\">"
+                + "</form>"
+                + "<p>If you click the \"Submit\" button, the form-data will be sent to a page called \"/results\".</p>"
+                + "</body>"
+                + "</html>";
+        return pageContent;
+    }
+
+    private static String resultsPage(Request req, Response res) {
+        return req.queryParams("firstname") + " " +
+                req.queryParams("lastname");
+    }
+
     
     static int getPort(){
         if (System.getenv("PORT")!=null){

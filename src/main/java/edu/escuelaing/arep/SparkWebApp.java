5d41becd3a7b990e318d49529ad72b3c90650a2c
camilavieda04 <sarah.vieda@mail.escuelaing.edu.co>
2020-01-30 17:38:21 -0500

    javadoc

--- src/main/java/edu/escuelaing/arep/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/SparkWebApp.java
@@ -13,8 +13,10 @@ import spark.Response;
 
 public class SparkWebApp {
 	private static ComplexSystems c;
-
-
+	
+	/**
+	 * Este metodo utiliza metodos de SparkWeb y funciones lambda.
+	 */
 	public static void main (String[]args){
 		c = new ComplexSystems();
         port(getPort());
@@ -24,8 +26,12 @@ public class SparkWebApp {
         
 	}
 	
-
-	
+	/**
+	 * Este metodo recibe los datos que el usuario desea agregar a la Linked List
+	 * @param req
+	 * @param res
+	 * @return pageC
+	 */
 	private static String inputDataPage(Request req, Response res) {
 		String pageC
 			="<!DOCTYPE html>" 
@@ -42,6 +48,12 @@ public class SparkWebApp {
 		return pageC;
 	}
 	
+	/**
+	 * Este metodo retorna el valor de la media y la desviacion estandar de los datos ingresados en la Linked List
+	 * @param req
+	 * @param res
+	 * @return resp
+	 */
 	private static String resultDataPage(Request req, Response res) {
 		String resp;
 		String [] valores=req.queryParams("data").split(",");
@@ -49,8 +61,6 @@ public class SparkWebApp {
 			c.insertandoNodo(Double.parseDouble(i));
 		}
 		Double media = c.retornaMedia();
-		
-	
 		resp = "<!DOCTYPE html>" 
 				+"<html>"
 				+"<body>" 
@@ -62,7 +72,10 @@ public class SparkWebApp {
 		return resp;
 	
 	}
-	
+	 /**
+     * Este metodo lee el puerto predeterminado según lo especificado por la variable PORT 
+     *
+     */
 	static int getPort(){
         if (System.getenv("PORT")!=null){
             return Integer.parseInt(System.getenv("PORT"));
